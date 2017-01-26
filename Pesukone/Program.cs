7e3d857af002411f79e02e7a87c5a4c0078a52fc
K8284 <K8284@OHJE-330-WS-22>
2017-01-26 15:30:50 +0200

    Pesukone

---
+++ Pesukone/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesukone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesukone pesukone = new Pesukone();
+            pesukone.IsOn = true;
+            pesukone.Temperature = 60;
+            pesukone.Color = "White";
+            pesukone.Brand = "LG";
+            pesukone.Speed = 120;
+            pesukone.PrintData();
+            pesukone.ShutDown();
+
+
+
+
+
+        }
+    }
+}
---
+++ Pesukone/pesukone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesukone
+{
+    class Pesukone
+    {
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public void ShutDown()
+        {
+            Console.WriteLine("Pesukone on sammunut ");
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Power on : "+IsOn);
+            Console.WriteLine("- Brand : " + Brand);
+            Console.WriteLine("- Bolor : " + Color);
+            Console.WriteLine("- Temperature : " + Temperature);
+            Console.WriteLine("- Speed : " + Speed);
+        
+      
+        }
+
+    }
+}
